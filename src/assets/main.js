9eb37ffd0b1fd8fc13609489ac5618f5a71c2a52
GNUgeneration <eric@asciolla.com>
2017-01-07 09:46:51 -0500

    Commit my first try at the first step of the build.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+    $.ajax('https://www.codeschool.com/users/GNUgeneration.json', function() {
+	dataType: 'jsonp',
+	success: function(response) {
+	    addCourses(response.courses.completed);
+	}
+    });
+
+    function addCourses(courses) {
+	var $badges = $('#badges');
+
+	courses.forEach(function(course) {
+
+	    var $course = $('<div></div>', {
+		'class': 'course'
+	    }).appendTo($badges);
+
+	    $('<h3></h3>', {
+		text:course.title
+            }).appendTo($course);
+
+	    $('<img />', {
+		src: course.badge
+            }).appendTo($course);
+	    
+	    $('<div></div>', {
+		'class': 'btn btn-primary',
+		target: '_blank',
+		href: course.url,
+		text: 'See Course'
+            }).appendTo($course);
+	});
+    }
 
 });
